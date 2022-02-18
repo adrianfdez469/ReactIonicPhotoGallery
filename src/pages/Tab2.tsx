58fb9f39126ec9db70d641e2ab92862d6cf817ee
Adrian <adrianfdez469@gmail.com>
2022-02-17 21:24:41 -0500

    First commit

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,13 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonIcon,
+  IonFabButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet
+} from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons'
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+
+  const { takePhoto } = usePhotoGallery();
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +35,13 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonFab vertical="bottom" horizontal="center" slot="fixed" style={{marginBottom:'5%'}}>
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera} />
+          </IonFabButton>
+            
+          
+        </IonFab>
       </IonContent>
     </IonPage>
   );
