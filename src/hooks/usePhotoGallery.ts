42937f4e727876af1168cd9c662a12d552549bdd
Adrian <adrianfdez469@gmail.com>
2022-02-19 19:02:35 -0500

    Adding store and load photos functionality to the app

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,9 +11,42 @@ export interface UserPhoto {
   webviewPath?: string;
 }
 
+
+const PHOTO_STORAGE = 'photos';
 export function usePhotoGallery() {
 
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const { value } = await Storage.get({key: PHOTO_STORAGE});
+      const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+      for (let photo of photosInStorage) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photosInStorage);
+    };
+    loadSaved();
+  },[]);
+
+  const savePicture = async (photo: Photo, filename: string):Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      data: base64Data,
+      directory: Directory.Data,
+      path: filename
+    });
+    
+    return {
+      filepath: filename,
+      webviewPath: photo.webPath,
+    };
+  };
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -23,18 +56,36 @@ export function usePhotoGallery() {
     });
     
     const filename = new Date().getTime() + '.jpeg';
+    const newPhoto = await savePicture(photo, filename);
     const newPhotos = [
-      {
-        filepath: filename,
-        webviewPath: photo.webPath
-      },
+      newPhoto,
       ...photos
     ];
     setPhotos(newPhotos);
+    Storage.set({key: PHOTO_STORAGE, value: JSON.stringify(newPhotos)});
   }
+
+  
 
   return {
     photos,
     takePhoto
   };
 };
+
+export async function base64FromPath(path: string): Promise<string> {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result);
+      } else {
+        reject('method did not return a string');
+      }
+    };
+    reader.readAsDataURL(blob);
+  });
+}
